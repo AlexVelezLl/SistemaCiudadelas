b37980b0ddf5d0ec9c5742a72ff05f0416e0100c
joangie <joangie@192.168.0.5>
2019-06-22 18:18:20 -0500

    Implementacion clase visitante

---
+++ SistemaCiudadelas/src/Sistema/Visitante.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sistema;
+import Sesion.Residente;
+
+/**
+ *
+ * @author joangie
+ */
+public class Visitante {
+    private String nombre;
+    private String id;
+    private String correo;
+    private Residente residenteAVisitar;
+    private CodigoAcceso codigoAcceso; 
+    
+    
+    public Visitante(){
+    
+    }
+    
+    public Visitante(String nombre, String id, String correo, Residente residenteAVisitar, CodigoAcceso codigoAcceso) {
+        this.nombre = nombre;
+        this.id = id;
+        this.correo = correo;
+        this.residenteAVisitar = residenteAVisitar;
+        this.codigoAcceso = codigoAcceso;
+    }
+
+    public Visitante(String nombre, String id, String correo, Residente residenteAVisitar) {
+        this.nombre = nombre;
+        this.id = id;
+        this.correo = correo;
+        this.residenteAVisitar = residenteAVisitar;
+        this.codigoAcceso= null; 
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public Residente getResidenteAVisitar() {
+        return residenteAVisitar;
+    }
+
+    public CodigoAcceso getCodigoAcceso() {
+        return codigoAcceso;
+    }
+
+    public void setCodigoAcceso(CodigoAcceso codigoAcceso) {
+        this.codigoAcceso = codigoAcceso;
+    }
+    
+    
+}
