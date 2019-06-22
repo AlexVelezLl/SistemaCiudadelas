54f11e8bc7386e18983a7d0181e146cd093cc16b
joangie <joangie@192.168.0.5>
2019-06-22 18:24:08 -0500

    Implementacion clase CodigoAcceso

---
+++ SistemaCiudadelas/src/Sistema/CodigoAcceso.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sistema;
+import java.time.LocalDateTime; 
+
+/**
+ *
+ * @author joangie
+ */
+public class CodigoAcceso {
+    private LocalDateTime fechaIngreso; 
+    private boolean used; 
+    private String codigo; 
+    
+    public CodigoAcceso(){
+    
+    }
+
+    public CodigoAcceso(LocalDateTime fechaIngreso, String codigo){
+        this.fechaIngreso = fechaIngreso;
+        this.used = false;
+        this.codigo = codigo;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+    
+    
+}
