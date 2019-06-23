c0789415e2cfd0ff1bb0303e0588b342087b96c6
joangie <joangie@192.168.0.7>
2019-06-22 19:39:37 -0500

    agregando pin a residentes creados por default y corrigiendo ciertos detalles

--- SistemaCiudadelas/src/Sistema/SistemaCiudadelas.java
+++ SistemaCiudadelas/src/Sistema/SistemaCiudadelas.java
@@ -42,7 +42,7 @@ public SistemaCiudadelas(){
     public void inicializarSistema(){
     
     LocalDate fInicio = LocalDate.of(2015, 12, 1);
-    AdministradorCiudadela adm1 = new AdministradorCiudadela("Lupita Lopez","luloppez@gmail.com",fInicio,"0901921921","ciudAdmin1","jUQ!$190");
+    AdministradorDeCiudadela adm1 = new AdministradorDeCiudadela("Lupita Lopez","luloppez@gmail.com",fInicio,"0901921921","ciudAdmin1","jUQ!$190");
     usuarios.add(adm1); 
     Ciudadela c1 = new Ciudadela("Ciudad celeste","CORPACEL","0992426357001","SAMBORONDON , Via Samborondon",adm1);
     ciudadelas.add(c1);  
@@ -56,11 +56,11 @@ public SistemaCiudadelas(){
             }
         
     Residente resd1,resd2;
-    resd1= new Residente("Juan Perez","juanperez@gmail.com","0908204012","2586589",c1.getCasas().get(0),"resd1C1","jLP#@121");
+    resd1= new Residente("Juan Perez","juanperez@gmail.com","0908204012","09599","2586589",c1.getCasas().get(0),"resd1C1","jLP#@121");
     usuarios.add(resd1);
     c1.getCasas().get(0).setResidente(resd1);
   
-    resd2= new Residente("Juana Crespo","juanacrespo@gmail.com","0906080011","2584586", c1.getCasas().get(1),"resd2C1","aQH%!087");
+    resd2= new Residente("Juana Crespo","juanacrespo@gmail.com","0906080011","1235","2584586", c1.getCasas().get(1),"resd2C1","aQH%!087");
     usuarios.add(resd2);
     c1.getCasas().get(1).setResidente(resd2);
 
