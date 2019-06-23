9387163a0979aa7795c8d6ecf7a813140a047fe3
AlexVelez <CORE i7 ULTIMATE@ALEX_VELEZ>
2019-06-23 07:55:01 -0500

    Completando interfaz garita

--- SistemaCiudadelas/src/Interfaz/Garita.java
+++ SistemaCiudadelas/src/Interfaz/Garita.java
@@ -6,7 +6,7 @@
 package Interfaz;
 import java.util.Scanner; 
 import java.util.ArrayList;
-import Sistema.Usuario;
+import Simulacro.*;
 import Sistema.Ciudadela;
 /**
  *
@@ -18,24 +18,50 @@ public class Garita {
         sc = new Scanner(System.in);
     }
     public void iniciarSimulacro(ArrayList <Ciudadela> ciudadelas){
-        String nomCiud,hacer;
+        String nomCiud,hacer="";
         System.out.println("Escriba el nombre de la Ciudadela: ");
         nomCiud = sc.nextLine();
         Ciudadela c;
+        RegistroIngreso r;
         do{
             c = buscarCiud(nomCiud,ciudadelas);
             if(c!=null){
-                System.out.println("Que desea hacer?");
-                System.out.println("1)Ingresar por el punto de acceso de Residente");
-                System.out.println("2)Ingresar por el punto de acceso de Visitante");
-                System.out.println("3)Salir");
-                hacer = sc.nextLine();
-                switch(hacer){
-                    case "1":
-                        c.getPuntosAcceso[0]
-                }
+                do{
+                    System.out.println("Que desea hacer?");
+                    System.out.println("1)Ingresar por el punto de acceso de Residente");
+                    System.out.println("2)Ingresar por el punto de acceso de Visitante");
+                    System.out.println("3)Salir");
+                    hacer = sc.nextLine();
+                    switch(hacer){
+                        case "1":
+                            PuntoAcceso d = c.getPuntosAcceso()[0];
+                            r = d.comprobarAcceso();
+                            if(r!=null){
+                                c.setIngresos(c.getIngresos().add(r));
+                            }else{
+                                System.out.println("No se ha completado el ingreso con exito");
+                            }
+                            break;
+                        case "2":
+                            PuntoAcceso d = c.getPuntosAcceso()[1];
+                            r = d.comprobarAcceso();
+                            if(r!=null){
+                                c.setIngresos(c.getIngresos().add(r));
+                            }else{
+                                System.out.println("No se ha completado el ingreso con exito");
+                            }
+                            break;
+                        case "3":
+                            System.out.println("Saliendo...");
+                            break;
+                        default:
+                            System.out.println("No ha ingresado una opcion valida, por favor ingrese una opcion valida");
+                    }
+                }while(!hacer.equals("3"));
+            }else{
+                System.out.println("Ha ingresado una Ciudadela que no se encuentra en el sistema, por favor Ingrese una Ciudadela valida o escriba la palabra 'SALIR' para regresar");
             }
-        }while(c!=null);
+        }while(c==null&&!hacer.equals("Salir"));
         
     }
     private Ciudadela buscarCiud(String nombre, ArrayList <Ciudadela> ciudadelas){
