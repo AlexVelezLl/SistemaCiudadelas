6f7b9fcf086838ecb2b4fbf478381e77a70fb2a2
joangie <joangie@192.168.0.8>
2019-07-06 14:14:50 -0500

    Documentacion de vehiculo

--- SistemaCiudadelas/src/Sistema/Vehiculo.java
+++ SistemaCiudadelas/src/Sistema/Vehiculo.java
@@ -6,7 +6,7 @@
 package Sistema;
 
 /**
- *
+ *Clase que se modela como vehiculo
  * @author CORE i7 ULTIMATE
  */
 public class Vehiculo {
@@ -15,14 +15,25 @@ public class Vehiculo {
         this.matricula = matricula;
         this.nomProp = nomProp;
     }
-
+/**
+ * Obtener matricula
+ * @return matricula String con la matricula del vehiculo
+ */
     public String getMatricula() {
         return matricula;
     }
-
+/**
+ * Obtener nombre de propietario
+ * @return nomProp String con el nombre del propietario del vehiculo
+ */
     public String getNomProp() {
         return nomProp;
     }
+    /**
+     * Metodo equals que comprobara que es el mismo vehiculo si la matricula es igual
+     * @param obj Objeto con el que se comparara
+     * @return boolean true si son el mismo y false si no son el mismo vehiculo
+     */
     @Override
     public boolean equals(Object obj){
         if(obj!=null && obj instanceof Vehiculo){
